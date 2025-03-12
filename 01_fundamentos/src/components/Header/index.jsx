4348a299f31417d2a48c50d2578df8563e981f1e
DanielFreitassc <danielfreitassc@gmail.com>
2025-03-11 23:17:30 -0300

    Initial commit

--- 01_fundamentos/src/components/Header/index.jsx
+++
@@ -1,9 +0,0 @@
-import { HeaderContainer } from "./styles"
-
-export const Header = () => {
-    return(
-        <HeaderContainer>
-
-        </HeaderContainer>
-    )
-}
--- 01_fundamentos/src/components/Header/styles.js
+++
@@ -1,8 +0,0 @@
-import styled from "styled-components";
-export const HeaderContainer = styled.div`
-    background-color: var(--gray-800);
-    display: flex;
-    justify-content: center;
-    width: 100%;
-    padding: 1.25rem 0;
-`
